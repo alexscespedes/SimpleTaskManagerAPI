efb8020570b25a42c4cfc1352c866a3e4e34ad81
alexscespedes <alexandersenc99@gmail.com>
2025-07-01 18:48:56 -0400

    first commit

---
+++ Models/Task.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SimpleTaskManagerAPI.Models;
+
+public class Task
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public DateTime DueDate { get; set; }
+    public bool IsCompleted { get; set; } = false;
+}
